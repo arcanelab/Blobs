b69ab7b0edfaf5832b24cfa343527d5a645588b6
arcanelab <zoltan@arcanelab.com>
2018-08-04 22:55:46 +0200

    Added simple camera controls

---
+++ Assets/Scripts/PlayerMoveControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveControls : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position += Vector3.forward * v + Vector3.right * h;
+    }
+}
